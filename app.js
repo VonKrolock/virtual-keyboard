1328e266e2bf6cbd4e2207e31ec032d0b42477d0
Viachaslau Zharko <viachaslau.zharko@gmail.com>
2020-04-10 20:00:08 +0300

    feat:add eslint and fix code

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-let textArea = document.querySelector("#write");
-let key = document.querySelector("#keyboard");
-let keyTwo = document.querySelector("#keyboardTwo");
-let containerTwo = document.querySelector('#containerTwo');
-let charsUpperCase = document.querySelectorAll(".alt");
-let chars = document.querySelectorAll(".char");
-let shift = document.querySelector(".shift");
-let shiftTwo = document.querySelector(".shiftTwo");
-let caps = document.querySelector('.caps');
-let capsTwo = document.querySelector('.capsTwo');
-let del = document.querySelectorAll('.delete');
-let lang = document.querySelector('.lang');
-let langTwo = document.querySelector('.langTwo')
-let enter = document.querySelectorAll('.return');
-let tab = document.querySelectorAll('.tab');
+const textArea = document.querySelector('#write');
+const key = document.querySelector('#keyboard');
+const keyTwo = document.querySelector('#keyboardTwo');
+const containerTwo = document.querySelector('#containerTwo');
+const charsUpperCase = document.querySelectorAll('.alt');
+const chars = document.querySelectorAll('.char');
+const shift = document.querySelector('.shift');
+const shiftTwo = document.querySelector('.shiftTwo');
+const caps = document.querySelector('.caps');
+const capsTwo = document.querySelector('.capsTwo');
+const del = document.querySelectorAll('.delete');
+const lang = document.querySelector('.lang');
+const langTwo = document.querySelector('.langTwo');
+const enter = document.querySelectorAll('.return');
+const tab = document.querySelectorAll('.tab');
 let shiftOn = false;
 let capsOn = false;
 
 // Tab Event
-tab.forEach(a=>a.addEventListener('click',()=>textArea.innerHTML += '\t' ));
+tab.forEach((a) => a.addEventListener('click', () => { textArea.innerHTML += '\t'; }));
 
-//Enter event
-enter.forEach(a=>a.addEventListener('click',()=>textArea.innerHTML += '\n' ));
+// Enter event
+enter.forEach((a) => a.addEventListener('click', () => { textArea.innerHTML += '\n'; }));
 
 // Chars events
-chars.forEach(a => a.addEventListener('click',()=>{ textArea.innerHTML += a.innerHTML;}))
-charsUpperCase.forEach(a => a.addEventListener('click',()=>{ textArea.innerHTML += a.innerHTML;}))
+chars.forEach((a) => a.addEventListener('click', () => { textArea.innerHTML += a.innerHTML; }));
+charsUpperCase.forEach((a) => a.addEventListener('click', () => { textArea.innerHTML += a.innerHTML; }));
 
-//Del event
-del.forEach(a => a.addEventListener('click',()=> textArea.innerHTML = textArea.innerHTML.slice(0,-1).replace(/&nbsp;/g," ") ));
+// Del event
+del.forEach((a) => a.addEventListener('click', () => { textArea.innerHTML = textArea.innerHTML.slice(0, -1).replace(/&nbsp;/g, ' '); }));
 
-//Caps event
-caps.addEventListener('click',()=>{capsOn = !capsOn;
-            capsOn ? key.classList.add("caps") : key.classList.remove("caps")})
+// Caps event
+caps.addEventListener('click', () => {
+  capsOn = !capsOn;
+  // eslint-disable-next-line no-unused-expressions
+  capsOn ? key.classList.add('caps') : key.classList.remove('caps');
+});
 
-capsTwo.addEventListener('click',()=>{capsOn = !capsOn;
-            capsOn ? keyTwo.classList.add("capsTwo") : keyTwo.classList.remove("capsTwo")})  
+capsTwo.addEventListener('click', () => {
+  capsOn = !capsOn;
+  // eslint-disable-next-line no-unused-expressions
+  capsOn ? keyTwo.classList.add('capsTwo') : keyTwo.classList.remove('capsTwo');
+});
 
-//Shift events
-shift.addEventListener('click',()=>{shiftOn = !shiftOn;
-            shiftOn ? key.classList.add("shift") : key.classList.remove("shift")})
+// Shift events
+shift.addEventListener('click', () => {
+  shiftOn = !shiftOn;
+  // eslint-disable-next-line no-unused-expressions
+  shiftOn ? key.classList.add('shift') : key.classList.remove('shift');
+});
 
-shiftTwo.addEventListener('click',()=>{shiftOn = !shiftOn;
-            shiftOn ? keyTwo.classList.add("shiftTwo") : keyTwo.classList.remove("shiftTwo")})
+shiftTwo.addEventListener('click', () => {
+  shiftOn = !shiftOn;
+  // eslint-disable-next-line no-unused-expressions
+  shiftOn ? keyTwo.classList.add('shiftTwo') : keyTwo.classList.remove('shiftTwo');
+});
 
-//Language events
-lang.addEventListener('click',()=>{shiftOn = !shiftOn ? containerTwo.style.display = 'block' : containerTwo.style.display = 'block'});
-langTwo.addEventListener('click',fooTwo);
-function fooTwo(){
-  if(shiftOn ===shiftOn){
+// Language events
+lang.addEventListener('click', () => { shiftOn = !shiftOn ? containerTwo.style.display = 'block' : containerTwo.style.display = 'block'; });
+// eslint-disable-next-line no-use-before-define
+langTwo.addEventListener('click', fooTwo);
+function fooTwo() {
+  // eslint-disable-next-line no-self-compare
+  if (shiftOn === shiftOn) {
     containerTwo.style.display = 'none';
   }
 }
-
-
